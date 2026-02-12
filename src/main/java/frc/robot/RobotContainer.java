37990394c55e10b58a117b3e247685ea8d9c96cf
AlbertEinstein314 <harry971217lm@gmail.com>
2026-02-12 20:07:19 +0800

    0212 下班

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -81,7 +81,7 @@ public class RobotContainer {
         //         .onTrue(drivetrain.runOnce(drivetrain::seedFieldCentric));
 
         new JoystickButton(driver, ConsController.Button.BUTTON_Y.id)
-                .toggleOnTrue(new ShooterMaster(m_shooter));
+                .toggleOnTrue(new ShooterMaster(m_shooter,driver));
 
 
         new JoystickButton(operator, ConsController.Button.BUTTON_Y.id)
